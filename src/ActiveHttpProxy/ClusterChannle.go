1fa5100e35b2d558a4369e8dfc3c5168f1a7e41e
lei13720831568 <13720831568@139.com>
2015-05-18 16:00:29 +0800

    加入集群节点通讯代码，未考虑好如何实现节点通讯，未完全实现

--- src/ActiveHttpProxy/ClusterChannle.go
+++ src/ActiveHttpProxy/ClusterChannle.go
@@ -1,10 +1,82 @@
 package ActiveHttpProxy
 
-import ()
+import (
+	"fmt"
+	"net/rpc"
+	//	"net/rpc"
+	log "RollLoger"
+	"StoppableListener"
+	"net"
+)
 
 type ClusterChannle struct {
-	ch        chan bool
-	exit      chan bool
-	OtherHost map[string]string
-	Host      string
+	Port     string
+	sl       *StoppableListener.StoppableListener
+	Partners []string
+}
+
+func (p *ClusterChannle) FlushLog(req *string, res *string) error {
+	fmt.Println("FlushLog")
+	*res = "ok"
+	return nil
+}
+
+func (p *ClusterChannle) Reload(req *string, res *string) error {
+	fmt.Println("Reload")
+	*res = "ok"
+	return nil
+}
+
+func (p *ClusterChannle) CallPartner(cmd string) {
+
+	address, err := net.ResolveTCPAddr("tcp", "192.168.1.129:11123")
+	if err != nil {
+		panic(err)
+	}
+
+	conn, _ := net.DialTCP("tcp", nil, address)
+	defer conn.Close()
+
+	client := rpc.NewClient(conn)
+	defer client.Close()
+
+	var res string
+	err = client.Call("ClusterChannle.FlushLog", "haha", &res)
+	if err != nil {
+		t.Fatal(err)
+	} else {
+		t.Log(res)
+	}
+
+}
+
+func (p *ClusterChannle) Begin() {
+
+	go func() {
+		rpc.Register(p)
+
+		originalListener, err := net.Listen("tcp", ":"+p.Port)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Debug("start clusterChannle tcp ", p.Port)
+		p.sl, err = StoppableListener.New(originalListener)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for {
+			conn, err := p.sl.Accept()
+			if err != nil {
+				log.Error("rpc accept error ", err.Error())
+				return
+			}
+			go rpc.ServeConn(conn)
+		}
+	}()
+
+}
+
+func (p *ClusterChannle) Stop() {
+	p.sl.Stop()
 }
